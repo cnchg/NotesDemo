a149faef18a2ecf8d6ef392584b0d805a9f08120
“Ori <chandleretienne@gmail.com>
2016-12-13 02:07:58 -0600

    initial

--- app/src/main/java/com/tricloudcommunications/ce/notesdemo/MainActivity.java
+++ app/src/main/java/com/tricloudcommunications/ce/notesdemo/MainActivity.java
@@ -5,11 +5,21 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    ListView notesLV;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,14 +28,24 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        notesLV = (ListView) findViewById(R.id.notesListView);
+
+        ArrayList<String> myNotesList = new ArrayList<String>();
+        ArrayAdapter arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, myNotesList);
+        notesLV.setAdapter(arrayAdapter);
+
+        myNotesList.add("Get new Lecture");
+
+        notesLV.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                Log.i("List Item", Integer.toString(position));
+
             }
         });
+
+
     }
 
     @Override
